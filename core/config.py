5edc20309f8edb7559b1728445d02a61ea33eb62
who am i <61992699+dragonknftcommunity@users.noreply.github.com>
2025-05-21 09:27:04 +0700

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 
 USE_ALL_TOKEN_BALANCE = True
-RPC_URL = "https://carrot.megaeth.com/rpc"
+RPC_URL = "testnet.dplabs-internal.com"
 ROUTER_ADDRESS = Web3.to_checksum_address("0xa6b579684e943f7d00d616a48cf99b5147fc57a5")
 DELAY_SWAP = 2
 DELAY_ERROR = 5
@@ -10,22 +10,14 @@ SLIPPAGE = 0.01
 GAS_LIMIT = 300000
 GAS_PRICE = 0.0035
 GAS_MULTIPLIER = 1.1
-BASE_TOKEN = "ETH"
+BASE_TOKEN = "PHRS"
 MIN_NATIVE_BALANCE = 0.0001
 SWAP_PERCENTAGE = 0.3
 
 GTE_TOKENS = {
-    "MegaETH": {"address": Web3.to_checksum_address("0x10a6be7d23989D00d528E68cF8051d095f741145"), "decimals": 18},
-    "WETH": {"address": Web3.to_checksum_address("0x776401b9BC8aAe31A685731B7147D4445fD9FB19"), "decimals": 18},
-    "GTE": {"address": Web3.to_checksum_address("0x9629684df53db9E4484697D0a50C442B2BFa80A8"), "decimals": 18},
-    "USDC": {"address": Web3.to_checksum_address("0x8D635C4702bA38B1f1735E8e784C7265dcc0B623"), "decimals": 6},
-    "tkUSDC": {"address": Web3.to_checksum_address("0xfAf334E157175fF676911aDcF0964D7f54F2c424"), "decimals": 6},
-    "Kimchizuki": {"address": Web3.to_checksum_address("0xA626F15D10F2b30AF1fb0d017F20a579500B5029"), "decimals": 18},
-    "five": {"address": Web3.to_checksum_address("0xF512886BC6877B0740E8Ca0B3c12bb4cA602B530"), "decimals": 18},
-    "gte pepe": {"address": Web3.to_checksum_address("0xBBA08CF5ECE0cC21e1DEB5168746c001B123A756"), "decimals": 18},
-    "Enzo": {"address": Web3.to_checksum_address("0x9cd3a7b840464d83bee643bc9064d246375b07a3"), "decimals": 18},
-    "Nazdaq": {"address": Web3.to_checksum_address("0xd0ed4c2af51bb08c58a808b9b407508261a87f25"), "decimals": 18},
-    "Toast": {"address": Web3.to_checksum_address("0xc49ae2a62e7c18b7ddcab67617a63bf5182b08de"), "decimals": 18},
+    "PHRS": {"address": Web3.to_checksum_address("0xacb0366aaba8440277c4d0615bf4ac60b34cccd2"), "decimals": 18},
+    "USDC": {"address": Web3.to_checksum_address("0xad902cf99c2de2f1ba5ec4d642fd7e49cae9ee37"), "decimals": 18},
+    
     "ETH": {"address": None, "decimals": 18},
 }
 
